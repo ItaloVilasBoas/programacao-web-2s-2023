3f7f00972bbd56bca1770b7283d8055e9cf85b6a
Italo Vilas Boas <vpn-990882@adm.bancoamazonia.sa>
2023-08-16 17:24:53 -0300

    refactor: colocando variaveis dentro da string

--- atividade2/exercicio1.js
+++ atividade2/exercicio1.js
@@ -1,19 +1,19 @@
 var somaNum1 = 4;
 var somaNum2 = 5;
 var resSoma = somaNum1 + somaNum2;
-console.log("Resultado da soma entre " + somaNum1 + " e " + somaNum2 + " = " + resSoma);
+console.log(`Resultado da soma entre ${somaNum1} e ${somaNum2} = ${resSoma}`);
 
 var subNum1 = 232;
 var subNum2 = 42;
 var resSub = subNum1 - subNum2;
-console.log("Resultado da subtracao entre " + subNum1 + " e " + subNum2 + " = " + resSub);
+console.log(`Resultado da soma entre ${subNum1} e ${subNum2} = ${resSub}`);
 
 var multNum1 = 32;
 var multNum2 = 13;
 var resMult = multNum1 * multNum2;
-console.log("Resultado da multiplicacao entre " + multNum1 + " e " + multNum2 + " = " + resMult);
+console.log(`Resultado da soma entre ${multNum1} e ${multNum2} = ${resMult}`);
 
 var divNum1 = 244;
 var divNum2 = 12;
 var resDiv = divNum1 / divNum2;
-console.log("Resultado da divisao entre " + divNum1 + " e " + divNum2 + " = " + resDiv);
+console.log(`Resultado da soma entre ${divNum1} e ${divNum2} = ${resDiv}`);
