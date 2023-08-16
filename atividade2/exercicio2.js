ce0c33e0d571805890fca9c7c5412afb746df9ec
Italo Vilas Boas <vpn-990882@adm.bancoamazonia.sa>
2023-08-16 17:15:19 -0300

    feat: exercicio 2 e 3 atividade 2

--- atividade2/exercicio2.js
+++ atividade2/exercicio2.js
@@ -3,22 +3,27 @@ const matrizA = [
     [3, 4],
     [5, 6]
 ];
-const matrizB = [];
+const matrizB = transporArray(matrizA);
 
-
-console.log("Matriz A:")
+console.log("Matriz A:");
 matrizA.forEach(element => {
-    console.log(element)
+    console.log(element);
 });
-console.log("Matriz Transposta:")
-matrizA.forEach(linha => {
-    var lin = 0;
-    matrizB[lin] = [];
-    for (let col = 0; col < linha.length; col++) {
-        matrizB[lin].push(linha[col]);
-    }
-    lin++;
-});
+
+console.log("Matriz Transposta:");
 matrizB.forEach(element => {
-    console.log(element)
+    console.log(element);
 });
+
+function transporArray(matriz) {
+    const matrizTransposta = new Array(matriz[0].length).fill([]).map(()=>{return new Array(matriz.length)});
+    
+    for(let lin = 0; lin < matriz.length; lin++){
+        for(let col = 0; col < matriz[0].length; col++){
+            matrizTransposta[col][lin] = matriz[lin][col];
+        }
+    }
+    return matrizTransposta;
+}
+
+
---
+++ atividade2/exercicio3.js
@@ -0,0 +1,37 @@
+const matrizA = [
+    [1, 2, 3],
+    [-2, 0, 4]
+];
+const matrizB = [
+    [2, 3],
+    [0, 1],
+    [-1, 4]
+];
+
+if(matrizA[0].length != matrizB.length){
+    console.log("Não é possível calcular")
+} else{
+    const matrizResult = multiplicaMatrizes(matrizA, matrizB);
+    matrizResult.forEach(element => {
+        console.log(element);
+    });   
+}
+
+function multiplicaMatrizes(matrizA, matrizB) {
+    const matrizProduto = new Array(matrizB[0].length).fill([]).map(()=>{return new Array(matrizA.length)});;
+
+    for(let linA = 0; linA < matrizA.length; linA++){
+        for(let colB = 0; colB < matrizB[0].length; colB++){
+            let valMatrizNova = obtemValorElemMatrizMult(matrizA, matrizB, linA, colB);
+            matrizProduto[linA][colB] = valMatrizNova;
+        }
+    }
+    return matrizProduto;
+}
+function obtemValorElemMatrizMult(matrizA, matrizB, linA, colB){
+    let valMatrizNova = 0;
+    for(let colA = 0; colA < matrizA[0].length; colA++){
+        valMatrizNova += matrizA[linA][colA] * matrizB[colA][colB];
+    }
+    return valMatrizNova;
+}
